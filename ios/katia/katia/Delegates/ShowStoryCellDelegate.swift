cfd953157e862b67b08d453a98590cf4ffb48746
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-07-14 21:21:37 +0200

    feat(delegates): add show story cell delegate
    notify show story controller when a story is done playing

---
+++ ios/katia/katia/Delegates/ShowStoryCellDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ShowStoryCellDelegate.swift
+//  katia
+//
+//  Created by Hadji on 13/07/2020.
+//  Copyright © 2020 Hadji. All rights reserved.
+//
+
+import Foundation
+
+protocol ShowStoryCellDelegate {
+    func slideToNextStory(currentCell: ShowStoryCell)
+}
