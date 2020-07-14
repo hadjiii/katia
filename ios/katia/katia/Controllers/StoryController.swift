9ad1cd91540137db5227d1ada006d6b514f3f71e
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-07-14 21:27:33 +0200

    feat(story-controller): chnage video url

--- ios/katia/katia/Controllers/StoryController.swift
+++ ios/katia/katia/Controllers/StoryController.swift
@@ -25,8 +25,8 @@ class StoryController: UICollectionViewController, UICollectionViewDelegateFlowL
                 Story(username: "Username1", mediaType: MediaType.image, medialink: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/250px-Image_created_with_a_mobile_phone.png", date: "yesterday", status: Status.read)
             ],
             [
-                Story(username: "Username2", mediaType: MediaType.video, medialink: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", date: "yesterday", status: Status.read),
-                Story(username: "Username2", mediaType: MediaType.video, medialink: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", date: "yesterday", status: Status.read),
+                Story(username: "Username2", mediaType: MediaType.video, medialink: "https://katiapp.s3.amazonaws.com/test.mp4", date: "yesterday", status: Status.read),
+                Story(username: "Username2", mediaType: MediaType.video, medialink: "https://katiapp.s3.amazonaws.com/test.mp4", date: "yesterday", status: Status.read),
             ],
             [
                 Story(username: "Username3", mediaType: MediaType.image, medialink: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/250px-Image_created_with_a_mobile_phone.png", date: "yesterday", status: Status.unread),
