563a7da428c5a0ab56e47dd4f59179d90d53556a
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-07-25 02:07:21 +0200

    feat(discussion-controller): display last message for each discussion

--- ios/katia/katia/Controllers/DiscussionController.swift
+++ ios/katia/katia/Controllers/DiscussionController.swift
@@ -11,7 +11,8 @@ import UIKit
 private let reuseIdentifier = "discussionCell"
 
 class DiscussionController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-
+    var discussions = Data.getDiscussions()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,22 +23,25 @@ class DiscussionController: UICollectionViewController, UICollectionViewDelegate
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return discussions.capacity
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
+        let discussion = self.discussions[indexPath.item]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! DiscussionCell
+        cell.message.text = discussion.text
+        cell.date.text = discussion.date
 
         return cell
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let discussion = self.discussions[indexPath.item]
         let layout = UICollectionViewFlowLayout()
         let messageController = MessageController(collectionViewLayout: layout)
         navigationController?.pushViewController(messageController, animated: true)
         messageController.userName = "Username"
+        messageController.userId = discussion.senderId == 1 ? discussion.recipientId : discussion.senderId
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
