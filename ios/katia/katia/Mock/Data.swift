a83797e642aeda43113ec99f6749ebca411d49a7
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-07-25 02:06:22 +0200

    feat(mock): create  message data and functions for add and get

--- ios/katia/katia/Mock/Data.swift
+++ ios/katia/katia/Mock/Data.swift
@@ -94,4 +94,31 @@ class Data: NSObject {
             return readStory
         }
     }
+    
+    static var messages = [
+        Message(id: 1, senderId: 1, recipientId: 2, text: "Hello", date: "Saturday, 19 AM"),
+        Message(id: 2, senderId: 2, recipientId: 1, text: "Hi", date: "Saturday, 19 AM"),
+        Message(id: 3, senderId: 1, recipientId: 2, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", date: "Saturday, 19 AM"),
+        Message(id: 4, senderId: 2, recipientId: 1, text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.", date: "Saturday, 19 AM"),
+        Message(id: 5, senderId: 1, recipientId: 2, text: "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.", date: "Saturday, 19 AM"),
+        Message(id: 6, senderId: 2, recipientId: 1, text: "Sed ut lectus porta augue tempor ornare. Nulla vel nisi sit amet lectus rhoncus aliquet. Donec placerat gravida laoreet.", date: "Saturday, 19 AM"),
+        Message(id: 7, senderId: 1, recipientId: 3, text: "Hello", date: "Saturday, 19 AM"),
+        Message(id: 8, senderId: 1, recipientId: 3, text: "Hello!!!", date: "Saturday, 19 AM"),
+    ]
+    
+    static func addMessage(_ message: Message) {
+        messages.append(message)
+    }
+    
+    static func getMessagesWithUserId(_ userId: Int) -> [Message] {
+        return messages.filter({$0.senderId == userId || $0.recipientId == userId})
+    }
+    
+    static func getDiscussions() -> [Message] {
+        let groupedDiscussion = Dictionary.init(grouping: messages) { (message) -> Int in
+            return message.senderId + message.recipientId
+        }
+        
+        return groupedDiscussion.map({$0.value.last!}) 
+    }
 }
