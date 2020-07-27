b887ce9b024cb19541df4dbe07df65b406133801
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-07-28 01:28:23 +0200

    feat(new-discussion-controller): setup collection view cells

--- ios/katia/katia/Controllers/NewDiscussionController.swift
+++ ios/katia/katia/Controllers/NewDiscussionController.swift
@@ -8,13 +8,20 @@
 
 import UIKit
 
-class NewDiscussionController: UICollectionViewController {
+private let reuseIdentifier = "newDiscussionCell"
+class NewDiscussionController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     override func viewDidLoad() {
         super.viewDidLoad()
         
         edgesForExtendedLayout = []
+        collectionView.register(NewDiscussionCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         collectionView.backgroundColor = UIColor(red: 36/255, green: 52/255, blue: 71/255, alpha: 1)
         collectionView.contentInset = UIEdgeInsets(top: 60, left: 0, bottom: 0, right: 0)
+        
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.scrollDirection = .vertical
+            flowLayout.minimumLineSpacing = 0
+        }
         
         view.addSubview(searchBar)
         searchBar.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
@@ -30,4 +37,23 @@ class NewDiscussionController: UICollectionViewController {
         sb.backgroundColor = .black
         return sb
     }()
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 70)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let layout = UICollectionViewFlowLayout()
+        let messageController = MessageController(collectionViewLayout: layout)
+        navigationController?.pushViewController(messageController, animated: false)
+    }
 }
