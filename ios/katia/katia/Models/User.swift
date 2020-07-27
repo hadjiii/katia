32615ee372007b9bd56af19d6dad27f2f486418a
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-07-28 01:30:35 +0200

    feat(models): create user model

---
+++ ios/katia/katia/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  katia
+//
+//  Created by Hadji on 28/07/2020.
+//  Copyright © 2020 Hadji. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var id: Int?
+    var name: String
+}
