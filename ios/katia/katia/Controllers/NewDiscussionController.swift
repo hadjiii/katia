15fe6e150ef72461a18f4609196356e7e1dffe1c
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-07-28 00:27:48 +0200

    feat(controllers): create new discussion controller

---
+++ ios/katia/katia/Controllers/NewDiscussionController.swift
@@ -0,0 +1,17 @@
+//
+//  NewDiscussionController.swift
+//  katia
+//
+//  Created by Hadji on 28/07/2020.
+//  Copyright © 2020 Hadji. All rights reserved.
+//
+
+import UIKit
+
+class NewDiscussionController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = UIColor(red: 36/255, green: 52/255, blue: 71/255, alpha: 1)
+    }
+}
