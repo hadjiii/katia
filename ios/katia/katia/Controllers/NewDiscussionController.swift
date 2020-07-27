5da2f9bf8fbab19f0e3ff11a82644cdd233df562
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-07-28 01:06:18 +0200

    feat(new-discussion-controller): setup navigation bar

--- ios/katia/katia/Controllers/NewDiscussionController.swift
+++ ios/katia/katia/Controllers/NewDiscussionController.swift
@@ -12,6 +12,22 @@ class NewDiscussionController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        edgesForExtendedLayout = []
         collectionView.backgroundColor = UIColor(red: 36/255, green: 52/255, blue: 71/255, alpha: 1)
+        collectionView.contentInset = UIEdgeInsets(top: 60, left: 0, bottom: 0, right: 0)
+        
+        view.addSubview(searchBar)
+        searchBar.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        searchBar.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        searchBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive = true
+        searchBar.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
+    
+    let searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.translatesAutoresizingMaskIntoConstraints = false
+        sb.placeholder = "Search an user"
+        sb.backgroundColor = .black
+        return sb
+    }()
 }
