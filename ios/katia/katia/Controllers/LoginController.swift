7eca2f1b06799ba77bb47116e6dab5c1912168d8
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-07-29 18:05:58 +0200

    feat(controllers): create login controller

---
+++ ios/katia/katia/Controllers/LoginController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginController.swift
+//  katia
+//
+//  Created by Hadji on 29/07/2020.
+//  Copyright © 2020 Hadji. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor(red: 20/255, green: 29/255, blue: 38/255, alpha: 1)
+    }
+}
