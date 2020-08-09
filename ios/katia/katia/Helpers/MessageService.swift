9dc6369670b8877627bac58516c4d2059f92d434
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-08-09 18:49:02 +0200

    feat(helpers): create message service

---
+++ ios/katia/katia/Helpers/MessageService.swift
@@ -0,0 +1,15 @@
+//
+//  MessageService.swift
+//  katia
+//
+//  Created by Hadji on 09/08/2020.
+//  Copyright © 2020 Hadji. All rights reserved.
+//
+
+import Foundation
+
+class MessageService {
+    static let shared = MessageService()
+    
+    private init() {}
+}
