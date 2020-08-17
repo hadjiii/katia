829edaf87c4a882cc0108527d37b9a41308d85b3
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-08-18 01:04:16 +0200

    feat(app): bootstrap servers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,19 @@
 const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+var io = socketIO(server);
 
 app.get("/", function(req, res) {
   res.send("Response from Katia backend");
 });
 
 const port = process.env.port || 3000;
-server.listen(3000);
+server.listen(port);
 console.log(`Server started at port ${port}`);
+
+io.on("connection", socket => {
+  console.log("new client", socket.id);
+});
