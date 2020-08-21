36bcbf1201763bb8b4b00e4912df9e231945cb50
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-08-21 11:50:21 +0200

    feat(models): create message model

---
+++ backend/src/models/message.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from 'mongoose';
+import { IMessage } from '../interfaces/message';
+import { UserSchema } from './user';
+
+export const MessageSchema = new mongoose.Schema(
+  {
+    _id: Schema.Types.ObjectId,
+    sender: { type: Schema.Types.ObjectId, ref: 'User' },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    media: { type: Schema.Types.ObjectId, ref: 'Media' },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model<IMessage & mongoose.Document>('Message', MessageSchema);
