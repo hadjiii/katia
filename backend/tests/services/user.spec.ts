7dac0e62baee6157c0f5da213fce9c16429ef6f1
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-08-21 16:34:54 +0200

    test(service): create user service tests

---
+++ backend/tests/services/user.spec.ts
@@ -0,0 +1,47 @@
+import "mocha"
+import { assert } from "chai"
+
+describe("User tests", () => {
+
+  /**
+   * Find user tests
+   */
+
+  it("Should find user", () => {
+    assert.fail("Find by id not implemented")
+    assert.fail("Find by username not implemented")
+  })
+
+  it("Should fail to find unexisted user", () => {
+    assert.fail("Find by id not implemented")
+    assert.fail("Find by username not implemented")
+  })
+
+  /**
+   * Update user tests
+   */
+
+  it("Should update user", () => {
+    assert.fail("Update by id not implemented")
+    assert.fail("Update by username not implemented")
+  })
+
+  it("Should fail to update unexisted user", () => {
+    assert.fail("Update by id not implemented")
+    assert.fail("Update by username not implemented")
+  })
+
+  /**
+   * Remove user tests
+   */
+
+  it("Should remove user", () => {
+    assert.fail("Remove by id Not implemented")
+    assert.fail("Remove by username not implemented")
+  })
+
+  it("Should fail to remove unexisted user", () => {
+    assert.fail("Remove by id not implemented")
+    assert.fail("Remove by username not implemented")
+  })
+})
