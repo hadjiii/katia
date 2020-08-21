64b452cb7bc77ce8c0051efd6c6507cebf06b0ab
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-08-21 12:03:32 +0200

    feat(loaders): create principal loader
    - load all loaders
    - add models into container

---
+++ backend/src/loaders/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongooseLoader from './mongoose';
+import expressLoader from './express';
+import Logger from './logger';
+import Container from 'typedi';
+import config from '../config';
+import {forkJoin, of, from, Observable} from 'rxjs';
+import {flatMap, map, switchMap, mergeMap} from 'rxjs/operators'
+
+export default async ({ expressApp }: { expressApp: express.Application }) => {
+  const db = await mongooseLoader(config.mongodb.databaseURL);
+  Logger.silly('Connection established to MongoDB');
+
+  const UserModel = await import("../models/user")
+  const MediaModel = await import("../models/media")
+  const DiscussionModel = await import("../models/discussion")
+  const StoryModel = await import("../models/story")
+  const MessageModel = await import("../models/message")
+
+  Container.set('userModel', UserModel.default);
+  Container.set('mediaModel', MediaModel.default);
+  Container.set('discussionModel', DiscussionModel.default);
+  Container.set('storyModel', StoryModel.default);
+  Container.set('messageModel', MessageModel.default);
+
+  Container.set('logger', Logger);
+
+  await expressLoader({ app: expressApp });
+};
