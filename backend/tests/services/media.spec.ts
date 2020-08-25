28fc537f68cf7a390908a9364d79507034b76eac
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-08-25 05:15:58 +0200

    test(services): create media tests

---
+++ backend/tests/services/media.spec.ts
@@ -0,0 +1,56 @@
+import "mocha"
+import { assert } from "chai"
+
+describe("MediaService tests", () => {
+  /**
+   * Create media tests
+   */
+
+  it("Should create media", () => {
+    assert.fail("Not implemented")
+  })
+
+  it("Should fail to create media", () => {
+    assert.fail("Find by id not implemented")
+  })
+
+  /**
+   * Find media tests
+   */
+
+  it("Should find media", () => {
+    assert.fail("Find by id not implemented")
+    assert.fail("Find by owner id not implemented")
+  })
+
+  it("Should fail to find unexisted media", () => {
+    assert.fail("Find by id not implemented")
+    assert.fail("Find by owner id not implemented")
+  })
+
+  /**
+   * Update media tests
+   */
+
+  it("Should update media", () => {
+    assert.fail("Update by id not implemented")
+  })
+
+  it("Should fail to update unexisted media", () => {
+    assert.fail("Update by id not implemented")
+  })
+
+  /**
+   * Remove media tests
+   */
+
+  it("Should remove media", () => {
+    assert.fail("Remove by id not implemented")
+    assert.fail("Remove by owner id not implemented")
+  })
+
+  it("Should fail to remove unexisted media", () => {
+    assert.fail("Remove by id not implemented")
+    assert.fail("Remove by owner id not implemented")
+  })
+})
