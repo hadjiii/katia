b773ceee31a1b9a94438ac6c54662efb5bd2523a
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-08-25 05:15:43 +0200

    test(services): create story tests

---
+++ backend/tests/services/story.spec.ts
@@ -0,0 +1,56 @@
+import "mocha"
+import { assert } from "chai"
+
+describe("StoryService tests", () => {
+  /**
+   * Create story tests
+   */
+
+  it("Should create story", () => {
+    assert.fail("Not implemented")
+  })
+
+  it("Should fail to create story", () => {
+    assert.fail("Find by id not implemented")
+  })
+
+  /**
+   * Find story tests
+   */
+
+  it("Should find story", () => {
+    assert.fail("Find by id not implemented")
+    assert.fail("Find by owner id not implemented")
+  })
+
+  it("Should fail to find unexisted story", () => {
+    assert.fail("Find by id not implemented")
+    assert.fail("Find by owner id not implemented")
+  })
+
+  /**
+   * Update story tests
+   */
+
+  it("Should update story", () => {
+    assert.fail("Update by id not implemented")
+  })
+
+  it("Should fail to update unexisted story", () => {
+    assert.fail("Update by id not implemented")
+  })
+
+  /**
+   * Remove story tests
+   */
+
+  it("Should remove story", () => {
+    assert.fail("Remove by id not implemented")
+    assert.fail("Remove by owner id not implemented")
+  })
+
+  it("Should fail to remove unexisted story", () => {
+    assert.fail("Remove by id not implemented")
+    assert.fail("Remove by owner id not implemented")
+  })
+})
