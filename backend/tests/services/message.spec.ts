33f05399198ecda026815edaae31b90fd1dfd3f8
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-08-25 10:55:25 +0200

    test(services): create message service tests

---
+++ backend/tests/services/message.spec.ts
@@ -0,0 +1,60 @@
+import "mocha"
+import { assert } from "chai"
+
+describe("MessageService tests", () => {
+  /**
+   * Create message tests
+   */
+
+  it("Should create message", () => {
+    assert.fail("Not implemented")
+  })
+
+  it("Should fail to create message", () => {
+    assert.fail("Find by id not implemented")
+  })
+
+  /**
+   * Find message tests
+   */
+
+  it("Should find message", () => {
+    assert.fail("Find by id not implemented")
+    assert.fail("Find by sender id not implemented")
+    assert.fail("Find by discussion id not implemented")
+  })
+
+  it("Should fail to find unexisted message", () => {
+    assert.fail("Find by id not implemented")
+    assert.fail("Find by sender id not implemented")
+    assert.fail("Find by discussion id not implemented")
+  })
+
+  /**
+   * Update message tests
+   */
+
+  it("Should update message", () => {
+    assert.fail("Update by id not implemented")
+  })
+
+  it("Should fail to update unexisted message", () => {
+    assert.fail("Update by id not implemented")
+  })
+
+  /**
+   * Remove message tests
+   */
+
+  it("Should remove message", () => {
+    assert.fail("Remove by id not implemented")
+    assert.fail("Remove by sender id not implemented")
+    assert.fail("Remove by discussion id not implemented")
+  })
+
+  it("Should fail to remove unexisted message", () => {
+    assert.fail("Remove by id not implemented")
+    assert.fail("Remove by sender id not implemented")
+    assert.fail("Remove by discussion id not implemented")
+  })
+})
