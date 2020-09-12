efa57b3f5cb721c7cc95df7ec3f25d4fdf433907
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2020-09-12 11:55:38 +0200

    fix(media-model): export type

--- backend/src/models/media.ts
+++ backend/src/models/media.ts
@@ -1,13 +1,12 @@
 import mongoose, { Schema } from 'mongoose';
 import { IMedia } from '../interfaces/media';
 
-export const StorySchema = new mongoose.Schema(
+export const MediaSchema = new mongoose.Schema(
   {
-    //_id: Schema.Types.ObjectId,
     owner: { type: Schema.Types.ObjectId, ref: 'User' },
     name: String,
   },
   { timestamps: true },
 );
 
-export default mongoose.model<IMedia & mongoose.Document>('Media', StorySchema);
+export default mongoose.model<IMedia & mongoose.Document>('Media', MediaSchema);
